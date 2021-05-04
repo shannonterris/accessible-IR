bc189cd2263e378f8610d8c2088a624a3204d1dc
shannonterris <shannonterriss@gmail.com>
2021-05-04 12:17:24 +1000

    Reformatting sending a message

--- client-exp/src/components/UserView.js
+++ client-exp/src/components/UserView.js
@@ -1,14 +1,32 @@
 import React, { useState } from "react";
-import { Container, Form } from "react-bootstrap";
+import { Container, Form, Button, InputGroup } from "react-bootstrap";
 import TextInput from "./TextInput";
 
 export default function UserView() {
-  const [innerText, setInnerText] = useState();
+  const [text, setText] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault(); // Prevent the auto refresh of page
+    return false;
+  }
+
   return (
     <div style={{ width: "250px" }} className="flex-column d-flex">
-      <Form>
-        <Form.Label>Text to Speech</Form.Label>
-        <TextInput innerText={innerText} setInnerText={setInnerText} />
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="m-2">
+          <InputGroup>
+            <Form.Control
+              as="textarea"
+              required
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              style={{ height: "75px", resize: "none" }}
+            ></Form.Control>
+            <InputGroup.Append>
+              <Button type="submit">Send</Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Form.Group>
       </Form>
     </div>
   );
