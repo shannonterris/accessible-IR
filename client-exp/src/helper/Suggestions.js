0e7a6e2d06d59ed650434aa0126bfa44303b4167
shannonterris <shannonterriss@gmail.com>
2021-09-22 23:22:06 +1000

    suggestions component ideas

---
+++ client-exp/src/helper/Suggestions.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+export default function Suggestions() {
+  // IDEAS:
+  // take input of the current tiles selected and current search term
+
+  // for text to speech
+  // if search term and tiles are nothing
+  // What would you like to see? What would you like to search for?
+  // If search term and one tile
+  // This is a/an "search term".
+  // Multiple tiles
+  // Which "search term" do you like? These are different "search term"
+
+  // for text info
+  //TODO: research what bing api can give back
+  // but maybe give defintion of search term
+  // title of where image tiles are from
+  // exert from image tile websites
+
+  // Suggestions could show up as bubbles under each input
+  // For longer suggestions like the text info, might want to make it a hover or when
+  // you click the button it enters more text and button just says "defintion"
+  return <div></div>;
+}
